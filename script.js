cc754b4c46a49073eddfb04a66baf48d379abe3e
Mai Banh <65269951+mtbanh@users.noreply.github.com>
2020-06-25 13:21:14 -0700

    attempt to create a change in color corresponding to the time

--- script.js
+++ script.js
@@ -18,7 +18,8 @@ $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 
 //TODO: need to create a function that will keep track of the current hour and change the style of the time block to reflect it
 //while loop(?) to check if the current hour is > than row; change style to be gray, else if (hour = row) change style to be white and else change style to be green
-var currentTime = moment().format("h:mm");
+var currentTime = moment().format("HH:mm");
+console.log(currentTime)
 
 //on click event save btn will do the following:
 //save the user-input value to local storage with the corresponding data-time
@@ -133,7 +134,7 @@ function color(){
         var hour = parseInt($(this).attr("id"));
         console.log(hour);
 
-        if (hour > currentTime){
+        if (hour < currentTime){
             $(this).addClass("past");
         } else if (hour === currentTime){
             $(this).removeClass("past");
